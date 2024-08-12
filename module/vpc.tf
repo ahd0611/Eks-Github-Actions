87c7f1c1479677d0c2cf5cfd1758d52677bfba5a
ahd0611 <161612006+ahd0611@users.noreply.github.com>
2024-08-12 19:10:18 +0530

    Update vpc.tf

--- module/vpc.tf
+++ module/vpc.tf
@@ -3,15 +3,14 @@ locals {
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.cidr-block
-  instance_tenancy     = "default"
+  cidr_block = var.cidr-block
+  instance_tenancy = "default"
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_support = true
 
   tags = {
     Name = var.vpc-name
-    Env  = var.env
-
+    Env = var.env
   }
 }
 
@@ -19,76 +18,69 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name                                          = var.igw-name
-    env                                           = var.env
+    Name = var.igw-name
+    Env = var.env
     "kubernetes.io/cluster/${local.cluster-name}" = "owned"
   }
 
-  depends_on = [aws_vpc.vpc]
+  depends_on = [ aws_vpc.vpc ]
 }
 
 resource "aws_subnet" "public-subnet" {
-  count                   = var.pub-subnet-count
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = element(var.pub-cidr-block, count.index)
-  availability_zone       = element(var.pub-availability-zone, count.index)
+  count = length(var.pub-cidr-block)
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = element(var.pub-cidr-block, count.index)
+  availability_zone = element(var.pub-availability-zone, count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name                                          = "${var.pub-sub-name}-${count.index + 1}"
-    Env                                           = var.env
+    Name =  "${var.pub-sub-name}- ${count.index + 1}"
+    Env = var.env
     "kubernetes.io/cluster/${local.cluster-name}" = "owned"
-    "kubernetes.io/role/elb"                      = "1"
+     "kubernetes.io/role/elb"                      = "1"
   }
-
-  depends_on = [aws_vpc.vpc,
-  ]
+  depends_on = [ aws_vpc.vpc ]
 }
 
 resource "aws_subnet" "private-subnet" {
-  count                   = var.pri-subnet-count
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = element(var.pri-cidr-block, count.index)
-  availability_zone       = element(var.pri-availability-zone, count.index)
+  count =  length(var.pri-cidr-block)
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = element(var.pri-cidr-block, count.index)
+  availability_zone = element(var.pri-availability-zone, count.index)
   map_public_ip_on_launch = false
 
   tags = {
-    Name                                          = "${var.pri-sub-name}-${count.index + 1}"
-    Env                                           = var.env
+    Name =  "${var.pri-sub-name}- ${count.index + 1}"
+    Env = var.env
     "kubernetes.io/cluster/${local.cluster-name}" = "owned"
-    "kubernetes.io/role/internal-elb"             = "1"
+     "kubernetes.io/role/elb"                      = "1"
   }
-
-  depends_on = [aws_vpc.vpc,
-  ]
+  depends_on = [ aws_vpc.vpc ]
 }
-
 
 resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.vpc.id
 
-  route {
+  route  {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
     Name = var.public-rt-name
-    env  = var.env
+    Env = var.env
   }
 
-  depends_on = [aws_vpc.vpc
-  ]
+  depends_on = [ aws_vpc.vpc ]
 }
 
-resource "aws_route_table_association" "name" {
-  count          = 3
-  route_table_id = aws_route_table.public-rt.id
-  subnet_id      = aws_subnet.public-subnet[count.index].id
+resource "aws_route_table_association" "public-rta" {
+    count = 3
+    route_table_id = aws_route_table.public-rt.id
+    subnet_id = aws_subnet.public-subnet[count.index].id
 
-  depends_on = [aws_vpc.vpc,
-    aws_subnet.public-subnet
-  ]
+    depends_on = [ aws_vpc.vpc, aws_subnet.public-subnet ]
+  
 }
 
 resource "aws_eip" "ngw-eip" {
@@ -96,74 +88,67 @@ resource "aws_eip" "ngw-eip" {
 
   tags = {
     Name = var.eip-name
+
   }
-
-  depends_on = [aws_vpc.vpc
-  ]
-
+depends_on = [ aws_vpc.vpc ]
 }
 
 resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.ngw-eip.id
-  subnet_id     = aws_subnet.public-subnet[0].id
+  subnet_id = aws_subnet.public-subnet[0].id
 
   tags = {
     Name = var.ngw-name
   }
+  depends_on = [ aws_vpc.vpc, aws_eip.ngw-eip ]
 
-  depends_on = [aws_vpc.vpc,
-    aws_eip.ngw-eip
-  ]
 }
 
 resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.ngw.id
   }
 
   tags = {
     Name = var.private-rt-name
-    env  = var.env
+    Env = var.env
   }
 
-  depends_on = [aws_vpc.vpc,
-  ]
+  depends_on = [ aws_vpc.vpc ]
 }
 
-resource "aws_route_table_association" "private-rt-association" {
-  count          = 3
-  route_table_id = aws_route_table.private-rt.id
-  subnet_id      = aws_subnet.private-subnet[count.index].id
+resource "aws_route_table_association" "private-rta" {
+    count = 3
+    route_table_id = aws_route_table.private-rt.id
+    subnet_id = aws_subnet.private-subnet[count.index].id
 
-  depends_on = [aws_vpc.vpc,
-    aws_subnet.private-subnet
-  ]
+    depends_on = [ aws_vpc.vpc, aws_subnet.public-subnet ]
 }
 
 resource "aws_security_group" "eks-cluster-sg" {
-  name        = var.eks-sg
-  description = "Allow 443 from Jump Server only"
+    name = var.eks-sg
+    description = " awllow 443 from jump server only"
+    vpc_id = aws_vpc.vpc.id
 
-  vpc_id = aws_vpc.vpc.id
+    ingress  {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] // It should be specific IP range
-  }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = var.eks-sg
-  }
+    tags = {
+      Name = var.eks-sg
+    }
+  
 }
