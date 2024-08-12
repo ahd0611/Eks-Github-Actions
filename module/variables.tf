bdb8784faae97c6fc516ea6d119147f03eb808fb
ahd0611 <161612006+ahd0611@users.noreply.github.com>
2024-08-12 19:09:56 +0530

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -3,34 +3,41 @@ variable "cidr-block" {}
 variable "vpc-name" {}
 variable "env" {}
 variable "igw-name" {}
-variable "pub-subnet-count" {}
+variable "ngw-name" {}
+variable "pub-subnet-count" {
+    type = number
+    default = 3
+}
 variable "pub-cidr-block" {
-  type = list(string)
+    type = list(string)
 }
 variable "pub-availability-zone" {
-  type = list(string)
+    type = list(string)
 }
 variable "pub-sub-name" {}
-variable "pri-subnet-count" {}
+variable "pri-subnet-count" {
+    type = number
+    default = 3
+}
 variable "pri-cidr-block" {
-  type = list(string)
+    type = list(string)
 }
 variable "pri-availability-zone" {
-  type = list(string)
+    type = list(string)
 }
 variable "pri-sub-name" {}
 variable "public-rt-name" {}
 variable "private-rt-name" {}
 variable "eip-name" {}
-variable "ngw-name" {}
 variable "eks-sg" {}
 
-#IAM
+
+# IAM
 variable "is_eks_role_enabled" {
-  type = bool
+    type = bool
 }
 variable "is_eks_nodegroup_role_enabled" {
-  type = bool
+    type = bool
 }
 
 # EKS
@@ -39,11 +46,12 @@ variable "cluster-version" {}
 variable "endpoint-private-access" {}
 variable "endpoint-public-access" {}
 variable "addons" {
-  type = list(object({
-    name    = string
-    version = string
-  }))
+    type = list(object({
+      name = string
+      version = string
+    }))
 }
+
 variable "ondemand_instance_types" {}
 variable "spot_instance_types" {}
 variable "desired_capacity_on_demand" {}
